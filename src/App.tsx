0c7cb9c2ce5f84e168daa5b13a61536075b5efe0
Helder <elder.goma@gmail.com>
2026-01-24 00:35:39 +0300

    Add toast notifications on add to cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import CatalogPage from './pages/CatalogPage/CatalogPage'
+import CartPage from './pages/CartPage/CartPage'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './widgets/Header/Header'
 
 function App() {
-
   return (
-    <>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<CatalogPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
 
-    </>
+      <ToastContainer
+        position="top-right"
+        autoClose={300}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        theme="light"
+      />
+    </BrowserRouter>
   )
 }
 
